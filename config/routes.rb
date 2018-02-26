13d2408ea526e2959f62fb614c709d7d276105b9
“reddrum” <anton.kranz@me.com>
2018-02-26 23:26:35 +0300

    Stripe integration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :games
   resource :subscription
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "games#index"
 end
